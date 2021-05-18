d35db649102b35741596e7c48871bacc69e10574
CSu <sufuturology@gmail.com>
2021-05-18 09:51:33 +0800

    Added param.py.

---
+++ param.py
@@ -0,0 +1,5 @@
+group_dict1 = {0:['LS180','LS220','LS375'],
+               1:['GShenFSU1.7','GShenFSU2.1','GShenNL3'],
+               2:['HShen','HShenH'],
+               3:['HSDD2','BHBL','BHBLP','HSTM1','HSTMA','HSFSG','HSIUF','HSNL3','SFHo','SFHx']
+              }
